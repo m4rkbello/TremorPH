0e28ee6a42a4003de9e5a4ddde50528ac98cc766
m4rkbello <markbello10041999@gmail.com>
2026-08-16 17:29:29 +0800

    Added

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello from Expo Router!</Text>
+    </View>
+  );
+}
