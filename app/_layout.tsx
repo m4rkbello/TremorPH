41948b315da4855de1a409d717970ef09e7645d5
m4rkbello <markbello10041999@gmail.com>
2026-08-16 17:29:22 +0800

    Added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,6 @@
-import { useEffect } from 'react';
-import { Stack, router } from 'expo-router';
-import { useAuthStore } from '../src/stores/authStore';
-import { ActivityIndicator, View } from 'react-native';
+// app/_layout.tsx
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  const { user, loading, loadUser } = useAuthStore();
-
-  useEffect(() => {
-    loadUser();
-  }, []);
-
-  if (loading) {
-    return (
-      <View className="flex-1 items-center justify-center">
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {!user ? (
-        <Stack.Screen name="(auth)" />
-      ) : (
-        <Stack.Screen name="(tabs)" />
-      )}
-    </Stack>
-  );
+  return <Stack />;
 }
