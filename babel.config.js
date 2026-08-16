e59fbe9b955068720577c2bcfb8b9243f2fcdcc8
m4rkbello <markbello10041999@gmail.com>
2026-08-16 09:25:00 +0800

    Added this shit

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'nativewind/babel',
-      'expo-router/babel',
-      'react-native-reanimated/plugin',
-    ],
+    presets: ['babel-preset-expo', 'nativewind/babel'],
   };
 };
