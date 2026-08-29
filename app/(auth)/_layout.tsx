c600a40c3a55048ab7f5710bbf7924e6ea46736c
m4rkbello <markbello10041999@gmail.com>
2026-08-29 23:46:11 +0800

    refactor

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,29 +1,5 @@
-import { useEffect } from 'react';
-import { Stack, useRouter, useSegments } from 'expo-router';
-import { useAuthStore } from '../../src/services/authService';
-import LoadingSpinner from '../../src/components/LoadingSpinner';
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  const { init, loading, user } = useAuthStore();
-  const segments = useSegments();
-  const router = useRouter();
-
-  useEffect(() => {
-    init();
-  }, []);
-
-  useEffect(() => {
-    if (loading) return;
-    const inAuthGroup = segments[0] === '(auth)';
-
-    if (!user && !inAuthGroup) {
-      router.replace('/(auth)/login');
-    } else if (user && inAuthGroup) {
-      router.replace('/(tabs)');
-    }
-  }, [user, loading, segments]);
-
-  if (loading) return <LoadingSpinner />;
-
+export default function AuthLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
