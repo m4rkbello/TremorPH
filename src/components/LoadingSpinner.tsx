45a938087a86c4aa7ac97ea977e4cd80925bb5e3
m4rkbello <markbello10041999@gmail.com>
2026-08-29 23:25:50 +0800

    refactor

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { ActivityIndicator, View } from 'react-native';
 
 export default function LoadingSpinner() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center bg-white">
       <ActivityIndicator size="large" color="#ef4444" />
     </View>
   );
